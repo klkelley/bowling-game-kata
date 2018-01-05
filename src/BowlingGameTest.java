a3104dcc9e494eeb8ccd35bf298a19497f10916e
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-05 08:29:34 -0600

    Add first test for a gutter game

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,4 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BowlingGameTest {
 
+    @Test
+    public void testGutterGame() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
 
 }
