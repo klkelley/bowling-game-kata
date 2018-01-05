6dd2ec69279c08307022496f3831314069a6d356
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-05 08:32:49 -0600

    Add failing test for testing game with only 1 pin per roll

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -13,4 +13,13 @@ public class BowlingGameTest {
         assertEquals(0, game.score());
     }
 
+    @Test
+    public void testAllOnes() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
+    }
+
 }
