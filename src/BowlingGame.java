fb76b2920f29189fd70e8992b06f841e742545b7
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-05 08:34:48 -0600

    Add instance variable to keep track of game score

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,10 +1,12 @@
 public class BowlingGame {
 
+    private int score = 0;
 
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
