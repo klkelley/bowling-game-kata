ce938dd72bd57bb34f5a35110ef3700e5f473135
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-05 09:18:06 -0600

    Add failing test for testing one strike

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -35,6 +35,16 @@ public class BowlingGameTest {
         assertEquals(16, game.score());
     }
 
+    @Test
+    public void testOneStrike() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(4);
+        rollMany(17, 0);
+
+        assertEquals(24, game.score());
+    }
+
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             game.roll(pins);
