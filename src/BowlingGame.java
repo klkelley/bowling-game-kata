430b05d8b5318e5209528a4512872193ec9e2483
Kara Kelley <karakelley@Karas-MacBook-Pro.local>
2018-01-05 08:47:12 -0600

    Add array to keep track of pins for each roll and instanc variable to track current roll in game

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,9 +1,12 @@
 public class BowlingGame {
 
     private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
         score += pins;
+        rolls[currentRoll++] = pins;
     }
 
     public int score() {
